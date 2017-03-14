33d509764d9100489ee0223ede42e34af7aaa7ce
Justin Ruessler <jruessler@gmail.com>
2017-03-14 18:17:25 -0500

    Create javascript objects in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,52 @@
 // Put all of your jQuery and JavaScript in this document.
+var book1 = {
+    "id": 1,
+    "name": "The Communist Manifesto",
+    "author": "Karl Marx"
+    "picture_url": "images/the-communist-manifesto.jpg",
+    "price": 50,
+    "selling_points": [
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    ]
+}
+
+var book2 = {
+    "id": 2,
+    "name": "The Communist Manifesto",
+    "author": "Karl Marx"
+    "picture_url": "images/the-communist-manifesto.jpg",
+    "price": 50,
+    "selling_points": [
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    ]
+}
+
+var book3 = {
+    "id": 3,
+    "name": "The Communist Manifesto",
+    "author": "Karl Marx"
+    "picture_url": "images/the-communist-manifesto.jpg",
+    "price": 50,
+    "selling_points": [
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    ]
+}
+
+var book4 = {
+    "id": 4,
+    "name": "The Communist Manifesto",
+    "author": "Karl Marx"
+    "picture_url": "images/the-communist-manifesto.jpg",
+    "price": 50,
+    "selling_points": [
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    ]
+}
