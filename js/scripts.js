d1700bc581890dacc275c81cdcc45f66de020d39
Travis Mowers <mowers@Traviss-MacBook-Air.local>
2017-03-14 18:55:20 -0500

    add correct jQuery on img

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 // Put all of your jQuery and JavaScript in this document.
 $( "#book1 h1" ).text( book1.name );
 $( "#book1 h2" ).text( book1.author );
-$( "#book1 img" ).attr( "src", "picture_url" );
+$( "#book1 img" ).attr( "src", book1.picture_url );
 $( "book1 h3" ).text( book1.price );
 $( "book1 ul" ).text( book1.selling_points );
 $( "book1 span" ).text( book1.id );
 
 $( "#book2 h1" ).text( book2.name );
 $( "#book2 h2" ).text( book2.author );
-$( "#book2 img" ).attr( "src", "picture_url" );
+$( "#book2 img" ).attr( "src", book2.picture_url );
 $( "book2 h3" ).text( book2.price );
 $( "book2 ul" ).text( book2.selling_points );
 $( "book2 span" ).text( book2.id );
 
 $( "#book3 h1" ).text( book3.name );
 $( "#book3 h2" ).text( book3.author );
-$( "#book3 img" ).attr( "src", "picture_url" );
+$( "#book3 img" ).attr( "src", book3.picture_url );
 $( "book3 h3" ).text( book3.price );
 $( "book3 ul" ).text( book3.selling_points );
 $( "book3 span" ).text( book3.id );
 
 $( "#book4 h1" ).text( book4.name );
 $( "#book4 h2" ).text( book4.author );
-$( "#book4 img" ).attr( "src", "picture_url" );
+$( "#book4 img" ).attr( "src", book4.picture_url );
 $( "book4 h3" ).text( book4.price );
 $( "book4 ul" ).text( book4.selling_points );
 $( "book4 span" ).text( book4.id );
